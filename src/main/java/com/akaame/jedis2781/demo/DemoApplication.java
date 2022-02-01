7ae9563826010d0aead2e5ee8d450642b701dea6
siddik.acil <siddik.acil@egym.com>
2022-02-01 20:20:15 +0100

    initial

---
+++ src/main/java/com/akaame/jedis2781/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.akaame.jedis2781.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akaame/jedis2781/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akaame.jedis2781.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
